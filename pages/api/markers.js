3aa4bbcc132f11a01b52c98ebf039a0e37e2452c
mahmedmushtaq <mahmedmushtaq296@gmail.com>
2023-03-19 13:31:28 +0500

    done basic clustering demo project

---
+++ pages/api/markers.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import dataDefault, { generate10KData } from "@/src/data";
+
+export default function handler(req, res) {
+  res.send({
+    ten_k_data: dataDefault,
+    twenty_k_data: [...dataDefault, ...generate10KData()],
+    hundred_k_data: [
+      ...dataDefault,
+      ...generate10KData(),
+      ...generate10KData(0.5),
+      ...generate10KData(1.2),
+      ...generate10KData(1.5),
+      ...generate10KData(1.7),
+      ...generate10KData(2.0),
+      ...generate10KData(2.2),
+      ...generate10KData(2.5),
+      ...generate10KData(3),
+    ],
+  });
+}
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const defaultCenter = { lat: 43.68, lng: -79.43 };
+export const API_KEY = process.env.NEXT_PUBLIC_MAP_KEY;
