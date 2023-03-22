7601f97a389593bd0799c232eb0436d2383a057d
mahmedmushtaq <mahmedmushtaq296@gmail.com>
2023-03-22 06:42:31 +0500

    updated component

--- pages/index.js
+++ pages/index.js
@@ -10,146 +10,18 @@ import usePlacesAutocomplete, {
 import ReactGoogleAutocomplete from "react-google-autocomplete";
 import PlacesAutocomplete from "@/src/components/PlacesAutoComplete";
 import { Box, Button, Typography } from "@mui/material";
+import GoogleMap from "@/src/components/GoogleMap";
+import { useRouter } from "next/router";
 
 const Marker = ({ children }) => children;
 
 export default function App() {
-  const mapRef = useRef();
-  const [bounds, setBounds] = useState(null);
-  const [zoom, setZoom] = useState(10);
-  const [points, setPoints] = useState([]);
-  const [panPosition, setPanPosition] = useState(defaultCenter);
-
-  const { clusters, supercluster } = useSupercluster({
-    points,
-    bounds,
-    zoom,
-    options: { radius: 75, maxZoom: 20 },
-  });
-
-  const loadAllPoints = useCallback(async () => {
-    const res = await fetch(
-      "https://m46ieddo4h.execute-api.us-east-1.amazonaws.com/Prod/markers"
-    ).then((res) => res.json());
-
-    // const points = [
-    //   ...dataDefault,
-    //   ...generate10KData(),
-    //   ...generate10KData(0.5),
-    //   ...generate10KData(1.2),
-    //   ...generate10KData(1.5),
-    //   ...generate10KData(1.7),
-    //   ...generate10KData(2.0),
-    //   ...generate10KData(2.2),
-    //   ...generate10KData(2.5),
-    //   ...generate10KData(3),
-    // ]
-    const points = res.items.map(
-      ({ device_name, latitude, longitude, uid }) => ({
-        type: "Feature",
-        properties: { cluster: false, crimeId: uid, category: longitude },
-        geometry: {
-          type: "Point",
-          coordinates: [parseFloat(longitude), parseFloat(latitude)],
-        },
-      })
-    );
-    setPoints(points);
-  }, []);
+  const router = useRouter();
+  const { arg } = router.query;
 
   return (
     <div>
-      <div style={{ height: "90vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: API_KEY, libraries: ["places"] }}
-          center={panPosition}
-          defaultZoom={10}
-          setBounds
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map }) => {
-            mapRef.current = map;
-            loadAllPoints();
-          }}
-          onChange={({ zoom, bounds }) => {
-            setZoom(zoom);
-            setBounds([
-              bounds.nw.lng,
-              bounds.se.lat,
-              bounds.se.lng,
-              bounds.nw.lat,
-            ]);
-          }}
-        >
-          {clusters.map((cluster, i) => {
-            const [longitude, latitude] = cluster.geometry.coordinates;
-            const { cluster: isCluster, point_count: pointCount } =
-              cluster.properties;
-
-            if (isCluster) {
-              return (
-                <Marker
-                  key={`cluster-${cluster.id}-${i}`}
-                  lat={latitude}
-                  lng={longitude}
-                >
-                  <div
-                    className="cluster-marker"
-                    style={{
-                      width: `${10 + (pointCount / points.length) * 20}px`,
-                      height: `${10 + (pointCount / points.length) * 20}px`,
-                    }}
-                    onClick={() => {
-                      const expansionZoom = Math.min(
-                        supercluster.getClusterExpansionZoom(cluster.id),
-                        20
-                      );
-                      mapRef.current.setZoom(expansionZoom);
-                      mapRef.current.panTo({ lat: latitude, lng: longitude });
-                    }}
-                  >
-                    {pointCount}
-                  </div>
-                </Marker>
-              );
-            }
-
-            return (
-              <Marker
-                key={`crime-${cluster.properties.crimeId}`}
-                lat={latitude}
-                lng={longitude}
-              >
-                <button className="crime-marker">
-                  <AddLocationIcon />
-                </button>
-              </Marker>
-            );
-          })}
-        </GoogleMapReact>
-      </div>
-      <Box
-        style={{
-          // left: "50%",
-          // right: "50%",
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          background: "white",
-          flexDirection: "column",
-          // transform: "translateX(-50%)",
-          alignItems: "center",
-          zIndex: 999999,
-        }}
-        sx={{ mt: 2 }}
-      >
-        <Typography variant="h6" mr={1}>
-          Search Places
-        </Typography>
-        <PlacesAutocomplete setPanPosition={setPanPosition} />
-        <Button variant="h6" mr={1}>
-          All Places
-        </Button>
-      </Box>
+      <GoogleMap requireFilter={arg === "filter"} />
     </div>
   );
 }
---
+++ src/components/GoogleMap.jsx
@@ -0,0 +1,156 @@
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import GoogleMapReact from "google-map-react";
+import useSupercluster from "use-supercluster";
+import { API_KEY, defaultCenter } from "@/src/constants";
+import AddLocationIcon from "@mui/icons-material/AddLocation";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import ReactGoogleAutocomplete from "react-google-autocomplete";
+import PlacesAutocomplete from "@/src/components/PlacesAutoComplete";
+import { Box, Button, Typography } from "@mui/material";
+
+const Marker = ({ children }) => children;
+
+export default function GoogleMap({ requireFilter }) {
+  const mapRef = useRef();
+  const [bounds, setBounds] = useState(null);
+  const [zoom, setZoom] = useState(10);
+  const [points, setPoints] = useState([]);
+  const [panPosition, setPanPosition] = useState(defaultCenter);
+
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom,
+    options: { radius: 75, maxZoom: 20 },
+  });
+
+  const loadAllPoints = useCallback(async () => {
+    const res = await fetch(
+      "https://m46ieddo4h.execute-api.us-east-1.amazonaws.com/Prod/markers"
+    ).then((res) => res.json());
+
+    console.log("res ", res);
+
+    // const points = [
+    //   ...dataDefault,
+    //   ...generate10KData(),
+    //   ...generate10KData(0.5),
+    //   ...generate10KData(1.2),
+    //   ...generate10KData(1.5),
+    //   ...generate10KData(1.7),
+    //   ...generate10KData(2.0),
+    //   ...generate10KData(2.2),
+    //   ...generate10KData(2.5),
+    //   ...generate10KData(3),
+    // ]
+    const points = res.items.map(
+      ({ device_name, latitude, longitude, uid }) => ({
+        type: "Feature",
+        properties: { cluster: false, crimeId: uid, category: longitude },
+        geometry: {
+          type: "Point",
+          coordinates: [parseFloat(longitude), parseFloat(latitude)],
+        },
+      })
+    );
+    setPoints(points);
+  }, []);
+
+  return (
+    <div>
+      <div style={{ height: !requireFilter ? "100vh" : "90vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: API_KEY, libraries: ["places"] }}
+          center={panPosition}
+          defaultZoom={10}
+          setBounds
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map }) => {
+            mapRef.current = map;
+            loadAllPoints();
+          }}
+          onChange={({ zoom, bounds }) => {
+            setZoom(zoom);
+            setBounds([
+              bounds.nw.lng,
+              bounds.se.lat,
+              bounds.se.lng,
+              bounds.nw.lat,
+            ]);
+          }}
+        >
+          {clusters.map((cluster, i) => {
+            const [longitude, latitude] = cluster.geometry.coordinates;
+            const { cluster: isCluster, point_count: pointCount } =
+              cluster.properties;
+
+            if (isCluster) {
+              return (
+                <Marker
+                  key={`cluster-${cluster.id}-${i}`}
+                  lat={latitude}
+                  lng={longitude}
+                >
+                  <div
+                    className="cluster-marker"
+                    style={{
+                      width: `${10 + (pointCount / points.length) * 20}px`,
+                      height: `${10 + (pointCount / points.length) * 20}px`,
+                    }}
+                    onClick={() => {
+                      const expansionZoom = Math.min(
+                        supercluster.getClusterExpansionZoom(cluster.id),
+                        20
+                      );
+                      mapRef.current.setZoom(expansionZoom);
+                      mapRef.current.panTo({ lat: latitude, lng: longitude });
+                    }}
+                  >
+                    {pointCount}
+                  </div>
+                </Marker>
+              );
+            }
+
+            return (
+              <Marker
+                key={`crime-${cluster.properties.crimeId}`}
+                lat={latitude}
+                lng={longitude}
+              >
+                <button className="crime-marker">
+                  <AddLocationIcon />
+                </button>
+              </Marker>
+            );
+          })}
+        </GoogleMapReact>
+      </div>
+      {requireFilter && (
+        <Box
+          style={{
+            // left: "50%",
+            // right: "50%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            background: "white",
+            flexDirection: "column",
+            // transform: "translateX(-50%)",
+            alignItems: "center",
+            zIndex: 999999,
+          }}
+          sx={{ mt: 2 }}
+        >
+          <Typography variant="h6" mr={1}>
+            Search Places
+          </Typography>
+          <PlacesAutocomplete setPanPosition={setPanPosition} />
+        </Box>
+      )}
+    </div>
+  );
+}
