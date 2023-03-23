24d78c26704af6e9251f51455fd1390811c700e9
mahmedmushtaq <mahmedmushtaq296@gmail.com>
2023-03-23 23:14:27 +0500

    removed google map script from document file

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script
-        defer
-        src={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`}
-      ></script>
+
       <body>
         <Main />
         <NextScript />
